08c1fbbd3f894abe88de70fc85cf221b90d10d99
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-03-26 15:26:24 +0900

    add page container for next.js with material-ui

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,69 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import JssProvider from 'react-jss/lib/JssProvider';
+
+import getPageContext from '../styles/getPageContext';
 
 class DocumentContainer extends Document {
-  render() {
-    return (
-      <html lang="ja">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, user-scalable=no" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
-  }
+  render = () => (
+    <html lang="ja">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=no" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+        { /* console.log(this.props) */ }
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </html>
+  )
 }
+
+DocumentContainer.getInitialProps = ({ renderPage }) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. page.getInitialProps
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the server with error:
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. page.getInitialProps
+  // 3. page.render
+
+  // Get the context of the page to collected side effects.
+  const pageContext = getPageContext();
+  const page = renderPage(Component => props => (
+    <JssProvider
+      registry={pageContext.sheetsRegistry}
+      generateClassName={pageContext.generateClassName}>
+      <Component pageContext={pageContext} {...props} />
+    </JssProvider>
+  ));
+
+  return {
+    ...page,
+    pageContext,
+    styles: (
+      <style
+        id="jss-server-side"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: pageContext.sheetsRegistry.toString() }} />
+    ),
+  };
+};
+
+
 export default DocumentContainer;
