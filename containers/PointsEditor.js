7a12a2da487b01dde4457e106f6e4a0819c2635d
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-03-27 19:11:16 +0900

    implement simple calculation distance

--- containers/PointsEditor.js
+++ containers/PointsEditor.js
@@ -3,14 +3,19 @@ import PropTypes from 'prop-types';
 import Input, { InputLabel } from 'material-ui/Input';
 import { FormControl } from 'material-ui/Form';
 
+import extractPoint from '../libs/extractPoint';
+
 class PointsEditor extends Component {
   static propTypes = {
+    id: PropTypes.string.isRequired,
     x: PropTypes.number,
     y: PropTypes.number,
+    onUpdatePoint: PropTypes.func,
   }
   static defaultProps = {
     x: 0,
     y: 0,
+    onUpdatePoint: () => {},
   }
   state = {
     value: `(${this.props.x},${this.props.y})`,
@@ -18,16 +23,21 @@ class PointsEditor extends Component {
 
   handleChange = (event) => {
     const { value } = event.currentTarget;
-    this.setState({ value });
+    const point = extractPoint(value);
+    this.setState({ value, point });
+    if (point) { this.props.onUpdatePoint(point); }
   }
 
   render = () => (
     <FormControl>
-      <InputLabel htmlFor="points">座標(x,y)</InputLabel>
+      <InputLabel htmlFor={this.props.id}>座標(x,y)</InputLabel>
       <Input
-        id="points"
+        id={this.props.id}
         value={this.state.value}
         onChange={this.handleChange} />
+      <p>
+        { this.state.point && `${this.state.point.x},${this.state.point.y}` }
+      </p>
     </FormControl>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import calcDistance, { displayDistance } from '../libs/calcDistance';
 
 import withMaterialUI from '../containers/withMaterialUI';
 import PointsEditor from '../containers/PointsEditor';
 
-const IndexPage = () => (
-  <div>
-    <h1>Index page</h1>
-    <form>
-      <PointsEditor x={750} y={750} />
-    </form>
-  </div>
-);
+class IndexPage extends Component {
+  state = {
+    distance: null,
+    startPoint: { x: 750, y: 750 },
+    endPoint: { x: 750, y: 750 },
+  }
+
+  handleUpdatePoint = startOrEnd => (newPoint) => {
+    const { startPoint, endPoint } = this.state;
+    let distance;
+    if (startOrEnd === 'startPoint') {
+      distance = calcDistance(newPoint, endPoint);
+    } else if (startOrEnd === 'endPoint') {
+      distance = calcDistance(startPoint, newPoint);
+    }
+    this.setState({
+      distance,
+      [startOrEnd]: newPoint,
+    });
+  }
+
+  render = () => {
+    const { distance, startPoint, endPoint } = this.state;
+    return (
+      <div>
+        <h1>Index page</h1>
+        <form>
+          <PointsEditor
+            id="startPoint"
+            onUpdatePoint={this.handleUpdatePoint('startPoint')}
+            x={startPoint.x}
+            y={startPoint.y} />
+          <PointsEditor
+            id="endPoint"
+            onUpdatePoint={this.handleUpdatePoint('endPoint')}
+            x={endPoint.x}
+            y={endPoint.y} />
+        </form>
+        <p>distance: { displayDistance(distance) }</p>
+      </div>
+    );
+  }
+}
 
 export default withMaterialUI(IndexPage);
